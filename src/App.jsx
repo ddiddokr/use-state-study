9f10812c0a0e70d55b05bcbfee332c3872528870
ddiddokr <ddiddokr1@naver.com>
2025-07-31 11:38:08 +0900

    item component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 // App.jsx
 import Counter from "./components/Counter.jsx";
+import InputText from "./components/InputText.jsx";
+import ItemAdd from "./components/ItemAdd.jsx";
 
 const App = () => {
   return (
     <div className="bg-red-100 min-h-screen flex justify-center items-center">
-      <Counter />
+      <ItemAdd />
     </div>
   );
 };
